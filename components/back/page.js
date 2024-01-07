2dc7dbc28f1baf606423dd63948a4ac59e063d36
sepsupriatnaa <asep93339@gmail.com>
2024-01-07 19:10:12 +0700

    menambahkan tombol back to top

--- components/back/page.js
+++ components/back/page.js
@@ -2,7 +2,8 @@ import Link from "next/link";
 
 const Back = () => {
     return (
-        <Link href='#home' className='fixed bg-orange-200 hover:bg-orange-300 p-3 z-[99] rounded-full shadow-md right-4 bottom-16'>
+        <Link href='#home' className='fixed bg-orange-200 hover:bg-orange-300 p-3 z-[99]
+        rounded-full shadow-md right-4 bottom-16 lg:right-8'>
             Top
         </Link>
     );
