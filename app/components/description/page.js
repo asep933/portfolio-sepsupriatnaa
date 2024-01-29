73811a4820fa9d2727c65d532cf4c189ce95014b
sepsupriatnaa <asep93339@gmail.com>
2024-01-29 15:30:26 +0700

    memperbaiki deskripsi

--- app/components/description/page.js
+++ app/components/description/page.js
@@ -13,49 +13,51 @@ const Description = ({
   setBackBlur,
 }) => {
   return (
-    <div
-      id="description"
-      className="center-element fixed z-[9999] h-[16rem] w-[24rem] rounded-md bg-slate-300 bg-opacity-75
-    p-5 shadow-md dark:bg-slate-300 dark:bg-opacity-90 lg:h-[24rem] lg:w-[38rem] lg:px-12 lg:shadow-lg"
-    >
-      <div className="space-y-3 dark:text-black">
-        {web && (
-          <button
-            onClick={() => {
-              setWeb(!web);
-              setBackBlur(!backBlur);
-            }}
-            className="absolute right-6 top-2 text-3xl transition duration-300 ease-in-out hover:scale-110"
-          >
-            x
-          </button>
-        )}
-        {prompt && (
-          <button
-            onClick={() => {
-              setPrompt(!prompt);
-              setBackBlur(!backBlur);
-            }}
-            className="absolute right-6 top-2 text-3xl transition duration-300 ease-in-out hover:scale-110"
-          >
-            x
-          </button>
-        )}
-        {uiux && (
-          <button
-            onClick={() => {
-              setUiux(!uiux);
-              setBackBlur(!backBlur);
-            }}
-            className="absolute right-6 top-2 text-3xl transition duration-300 ease-in-out hover:scale-110"
-          >
-            x
-          </button>
-        )}
+    <div className="max-w-full px-5">
+      <div
+        id="description"
+        className="center-element fixed z-[9999] h-[20rem] w-[18rem] rounded-md
+    bg-slate-300 bg-opacity-75 p-5 shadow-md dark:bg-slate-300 dark:bg-opacity-90 lg:h-[24rem] lg:w-[38rem] lg:px-12 lg:shadow-lg"
+      >
+        <div className="space-y-3 dark:text-black">
+          {web && (
+            <button
+              onClick={() => {
+                setWeb(!web);
+                setBackBlur(!backBlur);
+              }}
+              className="absolute right-6 top-2 text-3xl transition duration-300 ease-in-out hover:scale-110"
+            >
+              x
+            </button>
+          )}
+          {prompt && (
+            <button
+              onClick={() => {
+                setPrompt(!prompt);
+                setBackBlur(!backBlur);
+              }}
+              className="absolute right-6 top-2 text-3xl transition duration-300 ease-in-out hover:scale-110"
+            >
+              x
+            </button>
+          )}
+          {uiux && (
+            <button
+              onClick={() => {
+                setUiux(!uiux);
+                setBackBlur(!backBlur);
+              }}
+              className="absolute right-6 top-2 text-3xl transition duration-300 ease-in-out hover:scale-110"
+            >
+              x
+            </button>
+          )}
 
-        <div className="lg:space-y-3 lg:pt-16">
-          <h1 className=" text-xl font-semibold lg:text-2xl">{title}</h1>
-          <p className="px-4 lg:text-lg">{paragraph}</p>
+          <div className="lg:space-y-3 lg:pt-16">
+            <h1 className=" text-xl font-semibold lg:text-2xl">{title}</h1>
+            <p className="px-4 lg:text-lg">{paragraph}</p>
+          </div>
         </div>
       </div>
     </div>
